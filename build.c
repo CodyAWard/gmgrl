0e88ad2598efac953af1aa0f9c5bb17c8b32b78e
Cody Ward <codyward9@gmail.com>
2025-08-21 23:33:43 -0600

    build.c - adjustments to the meta build
    Adding support to the meta build program to forward arguments to the run
    command. Just a nicety for doing a rebuild/run at the same time.
    Probably needs some refinement, but this works for now when itterating
    on changes.

--- build.c
+++ build.c
@@ -16,7 +16,7 @@ i32 main(i32 arg_count, char* args[]) {
     trace_out("build: starting");
 
     bool should_run = false;
-
+    i32 last_used_arg = 1;
     if(arg_count > 1) {
         trace_out("build: with args");
         for(i32 i = 1; i < arg_count; i++) {
@@ -24,12 +24,14 @@ i32 main(i32 arg_count, char* args[]) {
 
             if(strcmp(args[i], "--run") == 0) {
                 should_run = true;
+                last_used_arg = i;
+            } else {
+                break;
             }
         }
     }
 
-
-    i32 result = system("cc src/main.c -o bin/gmgrl");
+    i32 result = system("cc src/*.c -o bin/gmgrl");
     if(result != 0) {
         trace_err("build: failed");
         return 1;
@@ -38,8 +40,25 @@ i32 main(i32 arg_count, char* args[]) {
     trace_out("build: done");
 
     if(should_run) {
-        trace_out("gmgrl: running");
-        result = system("./bin/gmgrl");
+        // pass the args to the program
+        char* concatenated_args;
+        i32 concatenated_args_len = 0;
+        for(i32 i = last_used_arg; i < arg_count; i++) {
+            concatenated_args_len += strlen(args[i]) + 1;
+        }
+        concatenated_args = mem_alloc(concatenated_args_len);
+        for(i32 i = last_used_arg; i < arg_count; i++) {
+            strcat(concatenated_args, args[i]);
+            if(i != arg_count - 1) {
+                strcat(concatenated_args, " ");
+            }
+        }
+
+        char* run_command = mem_alloc(strlen("./bin/gmgrl") + strlen(concatenated_args) + 1);
+        sprintf(run_command, "./bin/gmgrl %s", concatenated_args);
+        trace_out("gmgrl: running command: %s", run_command);
+
+        result = system(run_command);
         if(result != 0) {
             trace_err("gmgrl: failed");
             return 1;
